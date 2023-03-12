8e16073d448518c3096145216c3d581ae86a4410
max5961 <mmorrissey5961@gmail.com>
2023-03-12 17:55:48 -0400

    {

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-firstPwd = document.getElementById('firstPwd');
-secondPwd = document.getElementById('secPwd');
-submitBtn = document.getElementById('submitBtn');
-notMatch = document.querySelector('.notMatch');
+const firstPwd = document.getElementById('firstPwd');
+const secondPwd = document.getElementById('secPwd');
+const submitBtn = document.getElementById('submitBtn');
+const notMatch = document.querySelector('.notMatch');
+const tel = document.getElementById('tel');
+
+tel.addEventListener('input', () => {
+    if(/[a-zA-Z]/.test(tel.value.charAt(tel.value.length-1))){
+        tel.value = tel.value.substr(0,tel.value.length-1);
+    }
+//does not work with backspaces
+    if(tel.value.length == 3){
+        tel.value = `(${tel.value}) `
+    } else if(tel.value.length == 9){
+        tel.value = `${tel.value}-`
+    } else if(tel.value.length > 14){
+        tel.value =tel.value.substr(0,tel.value.length-1);
+    }
+})
 
 secondPwd.addEventListener('input', () => {
     if(firstPwd.value != secondPwd.value){
